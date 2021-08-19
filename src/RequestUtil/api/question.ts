f1afbb19b9580e07dc60e75c85727320b66e7447
yaolong.ye <470080502@qq.com>
2021-08-19 17:18:10 +0800

    暂存

--- src/RequestUtil/api/question.ts
+++ src/RequestUtil/api/question.ts
@@ -1,8 +1,8 @@
 import Http from '../axios/http';
 import qs from 'qs';
 
-const getQuestionList = (param) => Http.get('/api/getQuestionList', qs.stringify(param));
-const getQuestionTypeList = (param) => Http.get('/api/getQuestionTypeList', qs.stringify(param));
+const getQuestionList = (param: any) => Http.get('/api/getQuestionList', qs.stringify(param));
+const getQuestionTypeList = (param: any) => Http.get('/api/getQuestionTypeList', qs.stringify(param));
 
 export {
     getQuestionTypeList, // 获取题目类型列表
